524d496b76e5a78ed50872ff9b4d50bb0ccff9b3
johnpark0329 <johnpark0329@gmail.com>
2024-02-25 00:29:47 -0500

    Fixed layers of ContentView

--- Herricks Hub/ContentView.swift
+++ Herricks Hub/ContentView.swift
@@ -17,27 +17,54 @@ struct ContentView: View {
                     Text("Herricks Hub")
                         .font(.title)
                         .fontWeight(.bold)
-                        .frame(width: 300, height: 100).background(Color.white).foregroundColor(Color.blue)
+                        .frame(width: 300, height: 100)
+                        .background(Color.white)
+                        .foregroundColor(Color.blue)
                         
                     HStack{
                         NavigationLink(destination:GPA_Calculator_Page()){
-                            Text("GPA Calculator").multilineTextAlignment(.center).frame(width: 180, height: 130).background(Color.white).foregroundColor(Color.blue).cornerRadius(35)
+                            Text("GPA Calculator")
+                                .multilineTextAlignment(.center)
+                                .frame(width: 180, height: 130)
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(35)
                         }
                         NavigationLink(destination:Class_Notes_Page()){
-                            Text("Class Notes").multilineTextAlignment(.center).frame(width: 180, height: 130).background(Color.white).foregroundColor(Color.blue).cornerRadius(35)
+                            Text("Class Notes")
+                                .multilineTextAlignment(.center)
+                                .frame(width: 180, height: 130)
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(35)
                         }
                     }
                     HStack{
                         NavigationLink(destination:Teacher_Emails_Page()){
-                            Text("Teacher Emails").multilineTextAlignment(.center).frame(width: 180, height: 130).background(Color.white).foregroundColor(Color.blue).cornerRadius(35)
+                            Text("Teacher Emails")
+                                .multilineTextAlignment(.center)
+                                .frame(width: 180, height: 130)
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(35)
                         }
                         NavigationLink(destination:Clubs_Page()){
-                            Text("Clubs").multilineTextAlignment(.center).frame(width: 180, height: 130).background(Color.white).foregroundColor(Color.blue).cornerRadius(35)
+                            Text("Clubs")
+                                .multilineTextAlignment(.center)
+                                .frame(width: 180, height: 130)
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(35)
                         }
                     }
                     HStack{
                         NavigationLink(destination:Tutoring_Page()){
-                            Text("Zhou Tutoring").multilineTextAlignment(.center).frame(width: 180, height: 130).background(Color.white).foregroundColor(Color.blue).cornerRadius(35)
+                            Text("Tutoring")
+                                .multilineTextAlignment(.center)
+                                .frame(width: 180, height: 130)
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(35)
                         }
                     }
                 }
--- Herricks_Hub-gdinygkqrzlviyewpmighkmrzwmx/Build/Intermediates.noindex/Previews/Herricks Hub/Intermediates.noindex/Herricks Hub.build/Debug-iphonesimulator/Herricks Hub.build/Objects-normal/x86_64/ContentView.1.preview-thunk.swift
+++ Herricks_Hub-gdinygkqrzlviyewpmighkmrzwmx/Build/Intermediates.noindex/Previews/Herricks Hub/Intermediates.noindex/Herricks Hub.build/Debug-iphonesimulator/Herricks Hub.build/Objects-normal/x86_64/ContentView.1.preview-thunk.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 extension ContentView_Previews {
     @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
-        #sourceLocation(file: "/Volumes/PARK/Herricks Hub Application/Herricks Hub/Herricks Hub/ContentView.swift", line: 51)
+        #sourceLocation(file: "/Volumes/PARK/Herricks Hub Application/Herricks Hub/Herricks Hub/ContentView.swift", line: 78)
         ContentView()
     
 #sourceLocation()
@@ -19,30 +19,57 @@ extension ContentView {
                 Color(.blue)
                     .ignoresSafeArea()
                 VStack{
-                    Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].arg[0].value", fallback: "Herricks Hub"))
+                    Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].arg[0].value", fallback: "Herricks Hub"))
                         .font(.title)
                         .fontWeight(.bold)
-                        .frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].modifier[2].arg[0].value", fallback: 300), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].modifier[2].arg[1].value", fallback: 100)).background(Color.white).foregroundColor(Color.blue)
+                        .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].modifier[2].arg[0].value", fallback: 300), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[0].modifier[2].arg[1].value", fallback: 100))
+                        .background(Color.white)
+                        .foregroundColor(Color.blue)
                         
                     HStack{
                         NavigationLink(destination:GPA_Calculator_Page()){
-                            Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "GPA Calculator")).multilineTextAlignment(.center).frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130)).background(Color.white).foregroundColor(Color.blue).cornerRadius(__designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
+                            Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "GPA Calculator"))
+                                .multilineTextAlignment(.center)
+                                .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130))
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(__designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
                         }
                         NavigationLink(destination:Class_Notes_Page()){
-                            Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].arg[0].value", fallback: "Class Notes")).multilineTextAlignment(.center).frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130)).background(Color.white).foregroundColor(Color.blue).cornerRadius(__designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
+                            Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].arg[0].value", fallback: "Class Notes"))
+                                .multilineTextAlignment(.center)
+                                .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130))
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(__designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[1].arg[0].value.[1].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
                         }
                     }
                     HStack{
                         NavigationLink(destination:Teacher_Emails_Page()){
-                            Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "Teacher Emails")).multilineTextAlignment(.center).frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130)).background(Color.white).foregroundColor(Color.blue).cornerRadius(__designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
+                            Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "Teacher Emails"))
+                                .multilineTextAlignment(.center)
+                                .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130))
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(__designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
                         }
                         NavigationLink(destination:Clubs_Page()){
-                            Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].arg[0].value", fallback: "Clubs")).multilineTextAlignment(.center).frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130)).background(Color.white).foregroundColor(Color.blue).cornerRadius(__designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
+                            Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].arg[0].value", fallback: "Clubs"))
+                                .multilineTextAlignment(.center)
+                                .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130))
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(__designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[2].arg[0].value.[1].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
                         }
                     }
                     HStack{
                         NavigationLink(destination:Tutoring_Page()){
-                            Text(__designTimeString("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "Zhou Tutoring")).multilineTextAlignment(.center).frame(width: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130)).background(Color.white).foregroundColor(Color.blue).cornerRadius(__designTimeInteger("#3452.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
+                            Text(__designTimeString("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "Tutoring"))
+                                .multilineTextAlignment(.center)
+                                .frame(width: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 180), height: __designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[1].arg[1].value", fallback: 130))
+                                .background(Color.white)
+                                .foregroundColor(Color.blue)
+                                .cornerRadius(__designTimeInteger("#2207.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[1].arg[0].value.[3].arg[0].value.[0].arg[1].value.[0].modifier[4].arg[0].value", fallback: 35))
                         }
                     }
                 }
